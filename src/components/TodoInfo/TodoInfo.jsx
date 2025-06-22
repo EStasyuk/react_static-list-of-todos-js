37331c9a23c405ad3cbde4bf107b25cb372885c2
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-06-22 18:28:12 +0300

    initial

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,15 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+
+export const TodoInfo = ({ todo }) => {
+  return (
+    <article
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+  );
+};
